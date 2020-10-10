59b0d78014631bb9af37afc4420bd3e315a113cc
Estid Felipe Lozano Reyes <36607963+EstidLozano@users.noreply.github.com>
2020-10-10 18:23:15 -0500

    1er commit

---
+++ src/main/java/com/leets/util/Constants_CPaaSApi.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service.example2.util;
+
+/**
+ *
+ * @author umansilla
+ */
+public class Constants_CPaaSApi {
+    public final static String ACCOUNT_SID = "";
+	public final static String CPAAS_TOKEN_AUTH_BASIC = "Basic ";
+	public final static String FROM_SEND_SMS_NUMBER = "";
+}
---
+++ src/main/java/com/leets/util/Constants_CPaaSWorkShopApi.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service.example2.util;
+
+/**
+ *
+ * @author umansilla
+ */
+public class Constants_CPaaSWorkShopApi {
+    public final static String FQDN = "breeze2-196.collaboratory.avaya.com";
+	public final static String CUENTA = "";
+	public final static String TOKEN = "";
+}
